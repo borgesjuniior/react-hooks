1e13dacbbfc8b0e23064616ba74048ae55425d5e
Júnior <valdecyborgesjr@hotmail.com>
2021-06-10 23:00:08 -0300

    Add useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 interface User {
   name: string;
@@ -7,12 +7,21 @@ interface User {
 }
 
 function App() {
-  const [ user, setUser ] = useState<User>();
+  // Retorna um valor e uma função para atualizar o valor.
+  const [ user, setUser ] = useState<[User]>(); 
+  
+  /**
+  * O useMemo só recuperará o valor memoizado quando o array receber uma atualização. 
+  * Esta otimização ajuda a evitar cálculos caros em cada renderização.
+  * O primeiro parâmetro é a função a ser disparada e o segundo o array de depedências
+  */
+  const names = useMemo(() => user?.map(user => user.name).join(','), [user]);
+
+
 
   async function loadData() {
     const response = await fetch('https://api.github.com/users/borgesjuniior');
     const data = await response.json();
-    console.log(data);
 
     setUser(data);
   }
@@ -20,6 +29,9 @@ function App() {
   return (  
     <>
     <button onClick={loadData} type="submit">Carregar dados</button>
+    <li>{user?.login}</li>
+    {user && <img src={user?.avatar_url} alt="Foto" />}
+    
     </>
   );
 }
